876347386e62f55b3283378f222b807e42ba58da
Nik Stojkov <nik.stojkov@gmail.com>
2023-07-11 12:55:37 +0100

    yarn added tabulator

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,13 +3,10 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
 import "./src/jquery"
-import DataTable from "datatables.net-bs5";
+
+window.bootstrap = bootstrap
 
 
 $(function () {
   console.log("Hello from jQuery!")
 })
-
-// let table = DataTable.$('#example').DataTable({});
-// // render table
-// table.draw();
