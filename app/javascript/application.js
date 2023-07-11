d4deab6aac8c0354da9906c9249accbdf730e43e
Nik Stojkov <nik.stojkov@gmail.com>
2023-07-11 12:52:58 +0100

    jquery functioning globally, attempting to use tabulator instead of datatables

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,14 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+import "./src/jquery"
+import DataTable from "datatables.net-bs5";
+
+
+$(function () {
+  console.log("Hello from jQuery!")
+})
+
+// let table = DataTable.$('#example').DataTable({});
+// // render table
+// table.draw();
