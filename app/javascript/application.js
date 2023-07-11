3db9b146b3a8de6adb030b6dd00bb54ceaca2d36
Nik Stojkov <nik.stojkov@gmail.com>
2023-07-11 14:17:42 +0100

    added datatables JS, now adding jqueryUI

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,11 +2,16 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+import { TabulatorFull as Tabulator } from 'tabulator-tables'; //import Tabulator library
+
 import "./src/jquery"
+import 'jquery-ui'
 
-window.bootstrap = bootstrap
+//add Tabulator and bootstrap to the global
+window.bootstrap = bootstrap;
+window.Tabulator = Tabulator;
 
-
+// Testing JS is imported and jQuery is working
 $(function () {
   console.log("Hello from jQuery!")
 })
