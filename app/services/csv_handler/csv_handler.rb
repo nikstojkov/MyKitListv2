35a2e9f7ab4fbe38c3e6dd21c0fd5423746b6ebc
Nik Stojkov <nik.stojkov@gmail.com>
2023-09-28 15:26:43 +0200

    loading and reading csv data

--- app/services/csv_handler/csv_handler.rb
+++ app/services/csv_handler/csv_handler.rb
@@ -1,5 +1,13 @@
 require "csv"
 
-CSV.foreach("path/to/file.csv") do |row|
-  # use row here...
+CSV.foreach("app/services/csv_handler/TEST_DATA/Kit List 08_18 - Sheet1.csv") do |row|
+  # puts row.parse(string)
 end
+
+the_csv = CSV.read("app/services/csv_handler/TEST_DATA/Kit List 08_18 - Sheet1.csv")
+
+the_csv.each do |row|
+  # p row[0]
+end
+
+p the_csv[0].length
