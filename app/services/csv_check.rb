ea5d30184ebf21316fa5abdbebbd5535b2b53bac
Nik Stojkov <nik.stojkov@gmail.com>
2023-09-29 17:07:54 +0200

    tweaks needed

--- app/services/csv_check.rb
+++ app/services/csv_check.rb
@@ -2,31 +2,34 @@ p "conencted"
 class CSVCheck
 
   def self.run_checks(row)
-    is_field_null?(row)
+    blank_row?(row)
     check_fields(row)
     check_injections(row)
     something_else(row)
   end
 
-  def self.is_field_null?(row)
+  def self.status_report(method_name, status)
+    p "status report from #{method_name} says #{status}"
+  end
+
+  private
+
+  def self.blank_row?(row)
     row.fields.all?(&:blank?) ? p("BLANK FIELDS") : p("NOT BLANK FIELDS")
-    status_report("is_field_null?")
+    status_report("is_field_null?", "success")
   end
 
   def self.check_fields(row)
-    p "check fields method"
+    status_report("check_fields", "success")
   end
 
   def self.check_injections(row)
-    p "check injections method"
+    status_report("check_injections", "success")
   end
 
   def self.something_else(row)
-    p "something else method"
+    status_report("something_else", "success")
   end
 
-  def status_report(method_name, status)
-    p "status report from #{method_name} says #{status}"
-  end
 
 end
