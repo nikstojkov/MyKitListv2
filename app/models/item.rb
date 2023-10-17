4847dc1179eb08e134133347c3ed07f5f2779278
Nik Stojkov <nik.stojkov@gmail.com>
2023-10-17 22:31:56 +0100

    commented out seed file for time being, added some documentation for Item model/class

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,11 @@
+# frozen_string_literal: true
+
+# ----------As it stands----------
+# USER is optional
+# Attrs are dynamic to allow of easy ingest during development
+# No data cleaning is implemented yet
+
+# This is the constructor for items uploaded by a user.
 class Item
   include Mongoid::Document
   include Mongoid::Timestamps
